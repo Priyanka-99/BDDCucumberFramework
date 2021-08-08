f8016cad36901d8ea72f5ec619adce26e041aebb
Priyanka <86097836+priyankadhiman99@users.noreply.github.com>
2021-08-08 13:52:01 +0530

    New changes

---
+++ src/main/java/com/BddCucumberFramework/pageObjects/SkillraryDemoLoginPage.java
@@ -0,0 +1,45 @@
+package com.BddCucumberFramework.pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SkillraryDemoLoginPage {
+	public WebDriver driver;
+	
+	@FindBy(id="course")
+	private WebElement courseBtn;
+	
+	@FindBy(xpath="//div[@class='dropdown-content']//a[text()='Selenium Training']")
+	private WebElement seleniumTrainingEle;
+	
+	@FindBy(xpath="//a[text()='Contact Us']")
+	private WebElement contactUsEle;
+	
+	public SkillraryDemoLoginPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+		this.driver=driver;
+	}
+	
+	public AddToCartPage skillraryTrainingBtn() {
+		seleniumTrainingEle.click();
+		return new AddToCartPage(driver);
+	}
+
+	//getters
+	public WebElement getCourseBtn() {
+		return courseBtn;
+	}
+
+	public WebElement getSeleniumTrainingEle() {
+		return seleniumTrainingEle;
+	}
+
+	public WebElement getContactUsEle() {
+		return contactUsEle;
+	}
+	
+	
+	
+}
